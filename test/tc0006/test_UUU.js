8d32d42fefcf1593d7c52208403d24ba556e18be
Tim Berners-Lee <timbl+hg@w3.org>
2011-10-09 17:28:49 -0400

    General catch up varios minor things

--- test/tc0006/test_UUU.js
+++ test/tc0006/test_UUU.js
@@ -1,3 +1,6 @@
+//     Serialization tests
+//
+
 var tc0006Passed = true;
 
 var base = "http://dig.csail.mit.edu/hg/tabulator/raw-file/tip/chrome/content/test/tc0006/";
@@ -16,30 +19,46 @@ function testTC0006(showDetails) {
     var tests = [ ];
 
     tests.push({
-        'title': "Nested bnodes"
-        'input': """  :Fred :knows [ :name [ :first "Alice"; :second "Bill" ]]. """  
+        'title': "Nested bnodes",
+        'input': ' :Fred :knows [ :name [ :first "Alice"; :second "Bill" ]]. ',
+        'expected': '{_:n1 <http://dig.csail.mit.edu/hg/tabulator/raw-file/tip/chrome/content/test/tc0006/#first> "Alice" . _:n1 <http://dig.csail.mit.edu/hg/tabulator/raw-file/tip/chrome/content/test/tc0006/#second> "Bill" . _:n0 <http://dig.csail.mit.edu/hg/tabulator/raw-file/tip/chrome/content/test/tc0006/#name> _:n1 . <http://dig.csail.mit.edu/hg/tabulator/raw-file/tip/chrome/content/test/tc0006/#Fred> <http://dig.csail.mit.edu/hg/tabulator/raw-file/tip/chrome/content/test/tc0006/#knows> _:n0 .}'
+    },
+    {
+        'title': "Looped bnodes",
+        'input': ' _:a :knows [ :knows _:a ]. ',
+        'expected': '{_:n3 <http://dig.csail.mit.edu/hg/tabulator/raw-file/tip/chrome/content/test/tc0006/#knows> _:n2 . _:n2 <http://dig.csail.mit.edu/hg/tabulator/raw-file/tip/chrome/content/test/tc0006/#knows> _:n3 .}'
     });
     
     
     for(var i=0; i < tests.length; i++) {
         var test = tests[i];
         allResults += "<h2>" + test.title + "</h2>";
-        var result;
+        var result, expected;
         try {
             var kb = $rdf.graph();
             $rdf.parse(test.input, kb, base, 'text/n3');
-            result = kb.toNT()
+            result = kb.toNT();
+            result = result.replace(/\n/g, ' ').trim()
+            expected = test.expected.replace(/\n/g, ' ').trim()
         } catch(e) {
             result = "Runtime exception: "+e;
         }
-        if(result != test.expected) {
+        if(result != expected) {
+                for (var j=0; j<result.length; j++) {
+                    if (result[j] != expected[j]) {
+                        allResults += "Diff at "+j+": '"+(result[j])+"' ("+result.charCodeAt(j)+") vs '"
+                                    +test.expected[j]+"' ("+expected.charCodeAt(j)+").";
+                    }
+                }
+                
                 tc0006Passed = false;
                 styleResult =  failStyle;
         }
         else {
                 styleResult  =  passStyle;
         }
-        allResults += "<p>EXPECTED: " + escapeForXML(expected[i])+ "</p><p>RESULT: <span " + styleResult +">"+ escapeForXML(result) + "</p>"; /// encode for XML!!
+        allResults += "<p>EXPECTED: ("+test.expected.length+") " + escapeForXML(test.expected)+ "</p><p>RESULT ("+result.length+"): <span " + styleResult +">"+ escapeForXML(result) + "</p>";
+        
     }
     if(showDetails) return allResults;
     else return tc0006Passed;
