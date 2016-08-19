6553f5d9a5dac404002042bdf445e5d1ef71ac19
Dmitri Zagidulin <dzagidulin@gmail.com>
2016-08-19 11:51:05 -0400

    Move Formula.any() to IndexedFormula.any()

--- src/indexed-formula.js
+++ src/indexed-formula.js
@@ -269,7 +269,19 @@ class IndexedFormula extends Formula {
       this.add(quad.subject, quad.predicate, quad.object, quad.graph)
     })
   }
-
+  any (s, p, o, g) {
+    var st = this.anyStatementMatching(s, p, o, g)
+    if (st == null) {
+      return void 0
+    } else if (s == null) {
+      return st.subject
+    } else if (p == null) {
+      return st.predicate
+    } else if (o == null) {
+      return st.object
+    }
+    return void 0
+  }
   anyStatementMatching (subj, pred, obj, why) {
     var x = this.statementsMatching(subj, pred, obj, why, true)
     if (!x || x.length === 0) {
