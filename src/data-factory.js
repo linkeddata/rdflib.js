e7d450def60943004ea9554e2560e5fb104e8ac1
Dmitri Zagidulin <dzagidulin@gmail.com>
2017-07-18 13:51:03 -0400

    Update data factory .fetcher() signature

--- src/data-factory.js
+++ src/data-factory.js
@@ -18,8 +18,8 @@ function collection (elements) {
 function defaultGraph () {
   return new DefaultGraph()
 }
-function fetcher (store, timeout, async) {
-  return new Fetcher(store, timeout, async)
+function fetcher (store, timeout) {
+  return new Fetcher(store, { timeout })
 }
 function graph () {
   return new IndexedFormula()
