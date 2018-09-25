1b46f881afb4e9f8196271f6a2a8960c2d0e09ab
Ruben Verborgh <ruben@verborgh.org>
2018-09-25 18:33:36 +0200

    Use same build configuration as Mashlib.

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,43 @@
 const path = require('path')
 const MinifyPlugin = require('babel-minify-webpack-plugin')
 
-module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: path.join(__dirname, '/dist/'),
-    filename: 'rdflib.min.js',
-    library: '$rdf',
-    libraryTarget: 'umd'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  externals: {
-    '@trust/webcrypto': 'crypto',
-    'node-fetch': 'fetch',
-    'xmldom': 'window'
-  },
-  devtool: 'source-map',
-  plugins: [
-    new MinifyPlugin({ deadcode: false })
-  ]
+module.exports = (env, args) => {
+  return {
+    mode: 'production',
+    entry: [
+      './src/index.js'
+    ],
+    output: {
+      path: path.join(__dirname, '/dist/'),
+      filename: 'rdflib.min.js',
+      library: '$rdf',
+      libraryTarget: 'umd'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    optimization: {
+      minimizer: [
+        new MinifyPlugin({ deadcode: false }),
+      ]
+    },
+    externals: {
+      '@trust/webcrypto': 'crypto',
+      'fs': 'null',
+      'isomorphic-fetch': 'fetch',
+      'node-fetch': 'fetch',
+      'text-encoding': 'TextEncoder',
+      'whatwg-url': 'window',
+      'xhr2': 'XMLHttpRequest',
+      'xmldom': 'window',
+      'xmlhttprequest': 'XMLHttpRequest',
+    },
+    devtool: 'source-map'
+  }
 }
