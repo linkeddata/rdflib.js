04f7caf0955c22f995c4ecefea3b0c5cc80f7f3b
Ruben Verborgh <ruben@verborgh.org>
2020-04-06 21:30:07 +0200

    Make factory instances rdflib-specific.

--- tests/types/rdfjs-compatibility.ts
+++ tests/types/rdfjs-compatibility.ts
@@ -11,3 +11,15 @@ const blank1: RdfJs.BlankNode = factory.blankNode();
 const literal1: RdfJs.Literal = factory.literal('abc');
 const var1: RdfJs.Variable = factory.variable('var');
 const default1: RdfJs.DefaultGraph = factory.defaultGraph();
+
+// rdflib.js Term instances have rdflib.js-specific types
+const named2 = factory.namedNode('http://example.org/');
+named2.toCanonical();
+const blank2 = factory.blankNode();
+blank2.toCanonical();
+const literal2 = factory.literal('abc');
+literal2.toCanonical();
+const var2 = factory.variable('var');
+var2.toCanonical();
+const default2 = factory.defaultGraph();
+default2.toCanonical();
