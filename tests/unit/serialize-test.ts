d9a076bee775cf4f6ca15d785e4239e18ab95a51
Angelo Veltens <angelo.veltens@codecentric.de>
2021-03-18 16:00:16 +0100

    do not add .0 after 'e' notation

--- tests/unit/serialize-test.ts
+++ tests/unit/serialize-test.ts
@@ -94,6 +94,24 @@ describe('serialize', () => {
 `)
         });
 
+        it('"e" notation with negative exponent is serialized as-is', () => {
+            const doc = sym("https://doc.example");
+            const statement = st(
+                sym('https://subject.example'),
+                sym('https://predicate.example'),
+                lit("0.123e-2", undefined, sym("http://www.w3.org/2001/XMLSchema#double")),
+                doc
+            )
+            const kb = graph();
+            kb.add(statement)
+            const result = serialize(doc, kb, null, 'text/turtle')
+            expect(result).to.equal(`@prefix : <#>.
+
+<https://subject.example> <https://predicate.example> 0.123e-2 .
+
+`)
+        });
+
         it('capital "E" is serialized as-is', () => {
             const doc = sym("https://doc.example");
             const statement = st(
@@ -108,6 +126,42 @@ describe('serialize', () => {
             expect(result).to.equal(`@prefix : <#>.
 
 <https://subject.example> <https://predicate.example> 0.123E2 .
+
+`)
+        });
+
+        it('strings without dot but e notation are serialized as-is', () => {
+            const doc = sym("https://doc.example");
+            const statement = st(
+                sym('https://subject.example'),
+                sym('https://predicate.example'),
+                lit("123e2", undefined, sym("http://www.w3.org/2001/XMLSchema#double")),
+                doc
+            )
+            const kb = graph();
+            kb.add(statement)
+            const result = serialize(doc, kb, null, 'text/turtle')
+            expect(result).to.equal(`@prefix : <#>.
+
+<https://subject.example> <https://predicate.example> 123e2 .
+
+`)
+        });
+
+        it('strings without dot but e notation with negative exponent are serialized as-is', () => {
+            const doc = sym("https://doc.example");
+            const statement = st(
+                sym('https://subject.example'),
+                sym('https://predicate.example'),
+                lit("123e-2", undefined, sym("http://www.w3.org/2001/XMLSchema#double")),
+                doc
+            )
+            const kb = graph();
+            kb.add(statement)
+            const result = serialize(doc, kb, null, 'text/turtle')
+            expect(result).to.equal(`@prefix : <#>.
+
+<https://subject.example> <https://predicate.example> 123e-2 .
 
 `)
         });
