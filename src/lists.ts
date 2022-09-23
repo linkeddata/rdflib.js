3ddfc699e72f7dd8e491454d7ff89e98c40873d9
Tim Berners-Lee <timbl+github@w3.org>
2022-09-23 16:59:20 +0100

    tsc only

--- src/lists.ts
+++ src/lists.ts
@@ -1,3 +1,7 @@
+/* Lists form conversion
+*/
+
+
 import DataFactory from './factories/extended-term-factory'
 import jsonldParser from './jsonldparser'
 // @ts-ignore is this injected?
@@ -7,16 +11,24 @@ import { parseRDFaDOM } from './rdfaparser'
 import RDFParser from './rdfxmlparser'
 import sparqlUpdateParser from './patch-parser'
 import * as Util from './utils-js'
-import Formula from './formula'
+import Node from './node-internal'
+// import BlankNode from './blank-node'
+// import NamedNode from './named-node'
+import Collection from './collection'
+import Statement from './statement'
+// import Formula from './formula'
+import Store from './store'
 import { ContentType, TurtleContentType, N3ContentType, RDFXMLContentType, XHTMLContentType, HTMLContentType, SPARQLUpdateContentType, SPARQLUpdateSingleMatchContentType, JSONLDContentType, NQuadsContentType, NQuadsAltContentType } from './types'
-import { Quad } from './tf-types'
 
+// import { Quad } from './tf-types'
+
+import {BlankNode, NamedNode, Quad, Quad_Graph, Quad_Object, Quad_Predicate, Quad_Subject, Term,} from './tf-types'
 import Namespace from './namespace'
 
 const RDF  = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
 
 /**
- * Convert lists reified as rd
+ * Convert lists reified as rdf:first, rest
  * Normal method is sync.
  * Unfortunately jsdonld is currently written to need to be called async.
  * Hence the mess below with executeCallback.
@@ -24,28 +36,82 @@ const RDF  = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
  * @param doc - The document in which the conversion is done
  */
  // @@@@@@@ also fidn remaining rdf:nil empty list to ()
- 
-export default function convertFirstRestNil (
+
+/* Replace a given node for another node throughout a given document
+*
+* we do the predicate as well for complenesss though we don't expect Collections to use it
+*/
+export function substituteInDoc (store:Store, x:Term, y:Term, doc: NamedNode) {
+
+  for (const quad of store.statementsMatching(x as any, null, null, doc as any)) {
+    store.remove(quad)
+    store.add(new Statement(y as any, quad.predicate, quad.object, doc as any))
+  }
+  for (const quad of store.statementsMatching(null, x as any, null, doc) as any) {
+    store.remove(quad)
+    store.add(new Statement(quad.subject, y as any, quad.object, doc as any))
+  }
+  for (const quad of store.statementsMatching(null, null, x as any, doc) as any) {
+    store.remove(quad)
+    store.add(new Statement(quad.subject, quad.predicate, y as any, doc as any))
+  }
+}
+
+/* Change all lone rdf:nil nodes into empty Collections
+*/
+export function substituteNillsInDoc (store:Store, doc: NamedNode) {
+  const x = RDF('nil')
+  for (const quad of store.statementsMatching(x as any, null, null, doc as any)) {
+    store.remove(quad)
+    const y = new Collection()
+    store.add(new Statement(y as any, quad.predicate, quad.object, doc as any))
+  }
+  for (const quad of store.statementsMatching(null, x as any, null, doc) as any) {
+    store.remove(quad)
+    const y = new Collection()
+    store.add(new Statement(quad.subject, y as any, quad.object, doc as any))
+  }
+  for (const quad of store.statementsMatching(null, null, x as any, doc) as any) {
+    store.remove(quad)
+    const y = new Collection()
+    store.add(new Statement(quad.subject, quad.predicate, y as any, doc as any))
+  }
+}
+
+export function convertFirstRestNil (
   store: Store,
   doc: NamedNode | undefined,  // Do whole store
 ) {
-  let trash = []
-  function preceding (ele): Node[] {
+
+  function preceding (ele:BlankNode, listSoFar: Node[], trash: Quad[]): undefined {
     const pres = store.statementsMatching(null, RDF('rest'), ele, doc)
-    if (pres.length === 0) return { list: [], trash: [] }
-    if (pres.length !== 1) thow new Error(`Bad list structure: ${pres.length} pres at ${ele}`)
-    const pre = pres[0]
+    if (pres.length === 0) { // Head of the list
+      console.log(`Found a whole first/rest list: ${ele}, content: ${listSoFar}`)
+      const newList  = new Collection(listSoFar)
+
+      // Replace old list with new list:
+      substituteInDoc(store, newList, ele, doc)
+      // Remove the old form
+      store.remove(trash)
+    }
+    if (pres.length !== 1) throw new Error(`Bad list structure: ${pres.length} pres at ${ele}`)
+    const pre = pres[0].subject
+    if (pre.termType !== 'BlankNode')  throw new Error(`Bad list element node ${pre} type: ${pre.termType} `)
+
     const firsts = store.statementsMatching(pre, RDF('first'), null, doc)
-    if (firsts.length !== 1) thow new Error(`Bad list structure: rest but ${firsts.length} firsts at ${pre}`)
-    // trash = trash.concat(pres).concat(firsts)
-    return { list: preceding(pre).concat([firsts[0].object]), trash: pres.concat(firsts) }
+    if (firsts.length !== 1) throw new Error(`Bad list structure: rest but ${firsts.length} firsts at ${pre}`)
+    const value = firsts[0].object
+    preceding(pre, [value].concat(listSoFar as any), trash.concat(pres).concat(firsts))
+    return
   }
 
   const tails = store.statementsMatching(null, RDF('rest'), RDF('nil'), doc)
+  tails.forEach(tail => {
+    if (tail.subject.termType !== 'BlankNode')
+      throw new Error(`Bad list element node ${tail.subject} type: ${tail.subject.termType} `)
+    preceding(tail.subject, [], [])
 
-  const lists = tails.map(tail => {
-    { list, trash } = preceding
   })
-
+  substituteNillsInDoc(store, doc)
 
 }
