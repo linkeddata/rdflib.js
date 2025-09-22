d8608033ba1f2c253f34d9e6f22eec583f9c3f35
Angelo Veltens <angelo+github.com@veltens.org>
2025-09-22 20:40:38 +0200

    test: add unit tests for addDataCallback
    issue: https://github.com/linkeddata/rdflib.js/issues/708

---
+++ tests/unit/store-test.ts
@@ -0,0 +1,80 @@
+import chai, {expect} from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import {graph, parse, st, sym} from "../../src/";
+
+chai.use(sinonChai)
+
+describe('data callbacks', () => {
+
+    const statement1 = st(
+        sym("https://subject1.test"),
+        sym("https://predicate1.test"),
+        sym("https://object1.test"),
+        sym("https://graph1.test/")
+    )
+
+    const statement2 = st(
+        sym("https://subject2.test"),
+        sym("https://predicate2.test"),
+        sym("https://object2.test"),
+        sym("https://graph2.test/")
+    )
+
+    describe('addDataCallback', () => {
+
+        let store, callback;
+        beforeEach(() => {
+            store = graph()
+            callback = sinon.spy()
+            store.addDataCallback(callback)
+        });
+
+
+        it('callback is called when a statement is added', () => {
+            store.add(statement1)
+            expect(callback).to.have.been.calledWith(statement1)
+        });
+
+        it('callback is called when many statements are added separately', () => {
+            store.add(statement1)
+            store.add(statement2)
+            expect(callback).to.have.been.calledWith(statement1)
+            expect(callback).to.have.been.calledWith(statement2)
+        });
+
+        it('callback is called when many statements are added at once', () => {
+            store.addAll([statement1, statement2])
+            expect(callback).to.have.been.calledWith(statement1)
+            expect(callback).to.have.been.calledWith(statement2)
+        });
+
+        it('callback is called when data is parsed', () => {
+            parse(`
+            <https://subject.test> <https://predicate1.test> <https://object1.test>; 
+                                    <https://predicate2.test> <https://object2.test>; 
+            .`,
+                store, "https://graph.test/")
+            expect(callback).to.have.been.calledWith(st(
+                sym("https://subject.test"),
+                sym("https://predicate1.test"),
+                sym("https://object1.test"),
+                sym("https://graph.test/")
+            ))
+            expect(callback).to.have.been.calledWith(st(
+                sym("https://subject.test"),
+                sym("https://predicate2.test"),
+                sym("https://object2.test"),
+                sym("https://graph.test/")
+            ))
+        });
+
+        it('all callbacks are called, if there are multiple', () => {
+            const callback2 = sinon.spy()
+            store.addDataCallback(callback2)
+            store.add(statement1)
+            expect(callback).to.have.been.calledWith(statement1)
+            expect(callback2).to.have.been.calledWith(statement1)
+        });
+    });
+});
